dc722e3e49a9af720995d947e7e477e1a0209baf
Lautaro Murua <88483743+Latoomurua@users.noreply.github.com>
2021-10-29 13:56:45 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo!
-        </p>
-      </header>
+    <>
+    <div>
+      <NavBar />
     </div>
+    </>
   );
 }
 
 export default App;
-
-App
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,50 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+    return(
+    <div className='container'>
+    <div className='row'>
+    <div className='col-lg-12 col-xs-12'>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src='.\favicon.ico'></img></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <label><input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input></label>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    </div>
+    </div>
+    </div>
+    )
+};
+
+export default NavBar;

a3df2472080099d9bf3bafd904eba2067228db9f
Lautaro Murua <lato.murua@hotmail.com>
2021-11-04 17:20:10 -0300

    Inserte un componente que tiene como propiedad greeting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './componentes/NavBar'
 import NavCart from './componentes/NavCart/NavCart'
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <>
     <div>
       <NavBar />
+      <ItemListContainer greeting="Bienvenidos a mi APP en ReactJS"/>
     </div>
     </>
   );

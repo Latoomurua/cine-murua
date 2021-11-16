f44ced328c4d96c9253f88b88cdfc6dab64d4faf
Lautaro M <lato.murua@hotmail.com>
2021-11-16 15:09:14 -0300

    agregue data

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const peliculas = {
+    pelicula: [
+        {
+            id: '1',
+            name: 'Harry Potter: La piedra filosofal',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpeliculashdonline.com%2Fpeliculas%2Fharry-potter-y-la-piedra-filosofal%2F&psig=AOvVaw077Pgic0BXxQSuYh8PI2eZ&ust=1637111932254000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKjYpLPbm_QCFQAAAAAdAAAAABAD',
+        },
+        {
+            id: '2',
+            name: 'Harry Potter: La camara secreta',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomoacaba.com%2Fpeliculas%2Ffinal-harry-potter-y-la-camara-secreta%2F&psig=AOvVaw3O-TXC0RQP88yVYZUvx0VZ&ust=1637112080576000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCPCT7fPbm_QCFQAAAAAdAAAAABAD',
+        },
+        {
+            id: '3',
+            name: 'Harry Potter: El prisionero de Askaban',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FHarry-Potter-Prisoner-Azkaban-Poster%2Fdp%2FB00FEWFW5M&psig=AOvVaw3wtQVcF4pUB12CHJEErBCg&ust=1637112122244000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKj364fcm_QCFQAAAAAdAAAAABAT',
+        },
+        {
+            id: '4',
+            name: 'Harry Potter: El caliz de fuego',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sensacine.com%2Fpeliculas%2Fpelicula-53756%2F&psig=AOvVaw2k7235fOUHrEucl8wKmkPu&ust=1637112529675000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCMitvMndm_QCFQAAAAAdAAAAABAD',
+        },
+        {
+            id: '5',
+            name: 'Harry Potter: La orden del fenix',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpeliculashdonline.com%2Fpeliculas%2Fharry-potter-y-la-orden-del-fenix%2F&psig=AOvVaw3Sr1T20iStGSTI7-XgBSk2&ust=1637112561992000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCNDz2Njdm_QCFQAAAAAdAAAAABAD',
+        },
+        {
+            id: '6',
+            name: 'Harry Potter: El misterio del principe',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.abc.es%2Fplay%2Fpelicula%2Fharry-potter-y-el-misterio-del-principe-27037%2F&psig=AOvVaw2sLRsKe3XIRi0jI_o_B1_n&ust=1637112601767000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCPia1Ovdm_QCFQAAAAAdAAAAABAD',
+        },
+        {
+            id: '7',
+            name: 'Harry Potter: Las reliquias de la muerte Pt1',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sensacine.com%2Fpeliculas%2Fpelicula-126693%2F&psig=AOvVaw2OVTihwdsDwzppQpUJxozf&ust=1637112635334000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCIDQyfvdm_QCFQAAAAAdAAAAABAO',
+        },
+        {
+            id: '8',
+            name: 'Harry Potter: Las reliquias de la muerte Pt2',
+            price: '$599',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hobbyconsolas.com%2Freviews%2Fharry-potter-reliquias-muerte-parte-2-critica-pelicula-74064&psig=AOvVaw3KpZhXLgo_25SzlsGqmwWR&ust=1637112691368000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCNC1g5fem_QCFQAAAAAdAAAAABAD',
+        },
+    ],
+};
+
+export default peliculas
